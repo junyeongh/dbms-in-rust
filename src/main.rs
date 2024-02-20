61bc976a99f9de880565498c906a7d98784ab1d6
Junyeong Heo <hhjjyy1122@gmail.com>
2024-02-19 23:42:07 -0600

    Complete Part 1 - Introduction and Setting up the REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::{stdin, stdout, Write};
+
 fn main() {
-    println!("Hello, world!");
+    // use `loop` for infinite loop instead of while true
+    loop {
+        // print_prompt();
+        print!("rdbms> ");
+        let _ = stdout().flush(); // Flush the buffer immediately
+
+        let mut input = String::new();
+        let result = stdin().read_line(&mut input);
+        match result {
+            Ok(_) => {
+                // remove trailing newline
+                let input = input.trim_end();
+                if input == ".exit" {
+                    break;
+                } else {
+                    println!("Unrecognized command '{}'", input);
+                }
+            }
+            Err(error) => {
+                println!("Error: {}", error);
+            }
+        }
+    }
 }
